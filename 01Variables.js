82da926d1716eb23431f2848b02bfb938249ec7d
Acauxd <acauxd@gmail.com>
2025-10-22 18:51:41 -0500

    variables, condicionales

---
+++ 01Variables.js
@@ -0,0 +1,31 @@
+//tipos de variables
+const nombre = 'Amy';
+let apellido = 'Urrego';
+let dia ;
+console.log(dia);
+ 
+dia = 1; 
+console.log(nombre, apellido, dia  );
+
+//condicionales
+if (true) {
+    const nombre = 'Carolina';
+    console.log(nombre);
+};
+
+if (dia == 5) {
+    console.log('se cumplio la condicion');
+    
+} else {
+    console.log('no se cumplio la condicion');   
+}
+
+//bucles
+for (let i = 0; i <= 10 ; i++) {
+    if (i % 2 == 0) {
+        console.log(i + ' es par');    
+    } else {
+        console.log(i + ' es impar');
+    }
+    
+}
